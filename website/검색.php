b4afc9394bc1ee177b3258da35e03187a0adf649
ehdcjf4874 <ehdcjf4874@daum.net>
2021-12-11 21:46:13 +0900

    결과 출력화면 짤리는 문제 해결

--- website/검색.php
+++ website/검색.php
@@ -22,6 +22,12 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main2.css" />
+
+		<style>
+		.thumbnails h3 {
+				width:1000px;
+			}
+			</style>
 	</head>
 	<body>
 
