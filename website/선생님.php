6ffbbaba138774f539bd36e82d50cc6fdf8b49c2
gfdsa8 <74997073+gfdsa8@users.noreply.github.com>
2021-12-11 14:54:09 +0900

    선생님.php
    선생님에게 드릴 꽃 중에 감사의 의미를 담는 것도 있지만 카네이션도 준다고 하여 추가로 넣었습니다.
    또한, html을 추가하였으며, 꽃 사진도 추가하였습니다.

--- website/선생님.php
+++ website/선생님.php
@@ -2,16 +2,78 @@
 	$conn = mysqli_connect(
   		'localhost', 
   		'root',
-		'',
-  		'flower');
-		  echo '<h1>Database</h1>';
+		'9891',
+  		'test_2');
 
 		  $sql = "SELECT * 
-		  FROM flower.flower_db
-		  WHERE 꽃말 LIKE '%감사%'";
+		  FROM test_2.flower_db
+		  WHERE 꽃말 LIKE '%감사%' OR 
+			꽃이름 LIKE '%카네이션%' ORDER BY 월일 ASC";
 		  
 		  $result = mysqli_query($conn, $sql);
-			while($row = mysqli_fetch_array($result)){ 
-			  echo $row['월일'].' '.$row['꽃이름'].' '.$row['꽃말'].'<br>';
-			}
 ?>
+
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Visualize by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main2.css" />
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<span class="avatar"><img src="images/장미.png" alt="" /></span>
+						<h1>현재 <strong>"선생님"</strong>께 선물하면 좋은 꽃들을 보고 계십니다.<br />
+						다른 대상별 꽃을 보고 싶으면 "뒤로가기" 하시면 됩니다.</h1>
+					</header>
+
+				<!-- Main -->
+					<section id="main">
+
+						<!-- Thumbnails -->
+							<section class="thumbnails">
+								<div>
+									<a href="images/꽃/카네이션.png">
+										<img src="images/꽃/카네이션.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+											?></h3>
+									</a>
+								</div>
+								<div>
+									<a href="images/꽃/다알리아.png">
+										<img src="images/꽃/다알리아.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+											?></h3>
+									</a>
+								</div>
+							</section>
+
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p>&copy; Untitled. All rights reserved. Design: <a href="http://templated.co">TEMPLATED</a>. Demo Images: <a href="http://unsplash.com">Unsplash</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
