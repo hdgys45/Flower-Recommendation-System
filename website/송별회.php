64909befb3ff6e8e48a04806cd7bbee17a46083d
gfdsa8 <74997073+gfdsa8@users.noreply.github.com>
2021-12-11 19:13:49 +0900

    송별회.php <title> 수정
    <title>이 "Visualize by TEMPLATED"으로 되어 있어서 "상황별 - 송별회"로 바꿨습니다.

--- website/송별회.php
+++ website/송별회.php
@@ -24,7 +24,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Visualize by TEMPLATED</title>
+		<title>상황별 - 송별회</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link rel="stylesheet" href="assets/css/main2.css" />
