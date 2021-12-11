11fa6636dfddc8ab04074bfb99bea2191f89ae2b
gfdsa8 <74997073+gfdsa8@users.noreply.github.com>
2021-12-11 15:00:10 +0900

    친구.php
    html 추가하였고, 꽃 사진도 추가하였습니다.

--- website/친구.php
+++ website/친구.php
@@ -2,19 +2,120 @@
 	$conn = mysqli_connect(
   		'localhost', 
   		'root',
-		'',
-  		'flower');
-		  echo '<h1>Database</h1>';
+		'9891',
+  		'test_2');
 
 		  $sql = "SELECT * 
-		  FROM flower.flower_db
+		  FROM test_2.flower_db
 		  WHERE 꽃말 LIKE '%선물%' OR
-		  		꽃말 LIKE '%주의%' OR
-				꽃말 LIKE '%추억%'";
+				꽃말 LIKE '%추억%' ORDER BY 월일 ASC";
 				
 		  
 		  $result = mysqli_query($conn, $sql);
-			while($row = mysqli_fetch_array($result)){ 
-			  echo $row['월일'].' '.$row['꽃이름'].' '.$row['꽃말'].'<br>';
-			}
 ?>
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Visualize by TEMPLATED</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main2.css" />
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<span class="avatar"><img src="images/장미.png" alt="" /></span>
+						<h1>현재 <strong>"친구"</strong>에게 선물하면 좋은 꽃들을 보고 계십니다.<br />
+						다른 대상별 꽃을 보고 싶으면 "뒤로가기" 하시면 됩니다.</h1>
+					</header>
+
+				<!-- Main -->
+					<section id="main">
+
+						<!-- Thumbnails -->
+							<section class="thumbnails">
+								<div>
+									<a href="images/꽃/복수초.png">
+										<img src="images/꽃/복수초.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+											?></h3>
+									</a>
+									<a href="images/꽃/탱자나무.png">
+										<img src="images/꽃/탱자나무.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+										
+											?></h3>
+									</a>
+								</div>
+								<div>
+									<a href="images/꽃/라일락.png">
+										<img src="images/꽃/라일락.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+											?></h3>
+									</a>
+									<a href="images/꽃/과꽃.png">
+										<img src="images/꽃/과꽃.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+										
+											?></h3>
+									</a>
+								</div>
+								<div>
+									<a href="images/꽃/일일초.png">
+										<img src="images/꽃/일일초.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+											?></h3>
+									</a>
+									<a href="images/꽃/유칼리.png">
+										<img src="images/꽃/유칼리.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+										
+											?></h3>
+									</a>
+								</div>
+								<div>
+									<a href="images/꽃/후크시아.png">
+										<img src="images/꽃/후크시아.png" alt="" />
+										<h3><?php
+										$row = mysqli_fetch_array($result);
+										  echo '월일:',$row['월일'].'<br>'.'꽃이름:',$row['꽃이름'].'<br>'.'꽃말:',$row['꽃말'].'<br>';
+											?></h3>
+									</a>
+								</div>
+							</section>
+
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p>&copy; Untitled. All rights reserved. Design: <a href="http://templated.co">TEMPLATED</a>. Demo Images: <a href="http://unsplash.com">Unsplash</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
