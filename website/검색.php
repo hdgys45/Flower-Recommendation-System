2b4c33e6f92919776fb18cd1bd3bed3fb4b4622c
gfdsa8 <74997073+gfdsa8@users.noreply.github.com>
2021-12-11 21:04:11 +0900

    검색.php 수정
    html로 만들어서 <title> 수정하였고, 사진은 추가 못하겠지만 검색어와 관련된 꽃의 날짜와 이름, 꽃말등은 출력되게 만들었습니다.

--- website/검색.php
+++ website/검색.php
@@ -2,18 +2,66 @@
 	$conn = mysqli_connect(
   		'localhost', 
   		'root',
-		'',
-  		'flower');
+		'9891',
+  		'test_2');
 		  $search = $_GET['q'];
-		  echo '<h1>Database</h1>';
 
 		  $sql = "SELECT * 
-		  FROM flower.flower_db
-		  WHERE 꽃말 LIKE '%$search%' OR
-		  		꽃이름 LIKE '%$search%'";
+		  FROM test_2.flower_db
+		  WHERE 꽃말 LIKE '%$search%' OR	
+		  		꽃이름 LIKE '%$search%' ORDER BY 월일 ASC";
 		  
 		  $result = mysqli_query($conn, $sql);
-			while($row = mysqli_fetch_array($result)){ 
-			  echo $row['월일'].' '.$row['꽃이름'].' '.$row['꽃말'].'<br>';
-			}
 ?>
+
+
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>검색어와 관련된 꽃</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main2.css" />
+	</head>
+	<body>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<span class="avatar"><img src="images/장미.png" alt="" /></span>
+						<h1>현재 <strong>검색어</strong>에 관한 꽃들을 보고 계십니다.<br />
+						다른 추천 꽃을 보고 싶으면 "뒤로가기" 하시면 됩니다.</h1>
+					</header>
+
+				<!-- Main -->
+					<section id="main">
+
+						<!-- Thumbnails -->
+							<section class="thumbnails">
+								<div>
+								<h3><?php
+									while($row = mysqli_fetch_array($result)){ 
+										echo $row['월일'].' | '.$row['꽃이름'].' | '.$row['꽃말'].'<br>';
+										}
+									?></h3>
+							</section>
+
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<p>&copy; Untitled. All rights reserved. Design: <a href="http://templated.co">TEMPLATED</a>. Demo Images: <a href="http://unsplash.com">Unsplash</a>.</p>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
